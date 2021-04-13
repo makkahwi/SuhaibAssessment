34190fc90322129d866833f8b4299ae94d2607ed
makkahwi <makkahwi@live.com>
2021-04-13 19:59:37 +0800

    Final Submission

--- cryptoBalances.js
+++ cryptoBalances.js
@@ -2,71 +2,63 @@ const Papa = require("PapaParse");
 const fs = require("fs");
 const axios = require("axios");
 
-let file = fs.createReadStream('./data/sg.csv');
+let file = fs.createReadStream('./data/transactions.csv');
 
-let params = {
-  token: "BTC",
-  date: "2021/12/1"
-}
+let params = {  // Here are the parameters where to get to spacify the token or date by uncommenting the line and changing value
+  // token: "BTC",
+  // date: "12-31-2017"
+};
 
-// const epochToDateConverter = (date) => {
-//   axios.get(`https://showcase.api.linx.twenty57.net/UnixTime/tounix`, {
-//     params: {
-//       date: date,
-//     }
-//   })
-//   .then( result => {
-//     return result;
-//   })
-//   .catch( err => {
-//     return err;
-//   })
-// }
-
-const timestampToDateTimeConverter = (timestamp) => {
-  axios.get(`https://showcase.api.linx.twenty57.net/UnixTime/fromunix`, {
-    params: {
-      timestamp: timestamp,
-    }
-  })
-  .then( result => {
-    console.log(result)
-    return result
-  })
-  .catch( err => {
-    console.log(err);
-  })
-}
-
-
+let formattedDate;
 let balances = {};
 let tokens = [];
 
-Papa.parse(file, {
-  header: true,
-  step: result => {
-    if ((params.token && params.date && params.token === result.data.token && params.date === timestampToDateTimeConverter(parseint(result.data.timestamp))) || (params.token && !params.date && params.token === result.data.token) || (params.date && !params.token && params.date === timestampToDateTimeConverter(parseint(result.data.timestamp))) || (!params.token && !params.date))
-    {
-      if (!tokens.includes(result.data.token))
-      {
-        tokens.push(result.data.token);
-        balances[result.data.token] = 0;
-      }
+if (params.date)
+{
+  formattedDate = new Date(params.date);
+  formattedDate.setDate(formattedDate.getDate() + 2);
 
-      if (result.data.transaction_type === "DEPOSIT")
-      {
-        balances[result.data.token] += parseFloat(result.data.amount);
+  const unixToDateTimeConverter = () => {
+    axios.get(`https://showcase.api.linx.twenty57.net/UnixTime/tounix`, {
+      params: {
+        date: formattedDate,
       }
-      else
-      {
-        balances[result.data.token] -= parseFloat(result.data.amount);
-      }
-    }
-  },
-  complete: () => {
-    getUSDBalances()
-  }
-})
+    })
+    .then( res => {
+      Papa.parse(file, {
+        step: result => {
+          console.log(parseInt(res.data))
+          console.log(parseInt(result.data[0]))
+          if ((params.token && params.date && params.token === result.data.token && (parseInt(res.data) > parseInt(result.data[0]))) || (params.token && !params.date && params.token === result.data.token) || (params.date && !params.token && (parseInt(res.data) > parseInt(result.data[0]))) || (!params.token && !params.date))
+          {
+            if (!tokens.includes(result.data.token))
+            {
+              tokens.push(result.data.token);
+              balances[result.data.token] = 0;
+            }
+      
+            if (result.data.transaction_type === "DEPOSIT")
+            {
+              balances[result.data.token] += parseFloat(result.data.amount);
+            }
+            else
+            {
+              balances[result.data.token] -= parseFloat(result.data.amount);
+            }
+          }
+        },
+        complete: () => {
+          getUSDBalances()
+        }
+      })
+    })
+    .catch( err => {
+      return err;
+    })
+  };
+
+  unixToDateTimeConverter();
+}
 
 const getUSDBalances = () => {
   tokens.forEach(token => {
@@ -77,7 +69,7 @@ const getUSDBalances = () => {
       }
     })
     .then( result => {
-      fs.appendFile("./results.txt", JSON.stringify({[token]: "USD"+result.data.USD * balances[token]}), 'utf8', function(err, result) {
+      fs.appendFile("./results/noParameters.txt", JSON.stringify({[token]: "USD"+result.data.USD * balances[token]}), 'utf8', function(err, result) {
         if (err) console.log('error', err);
       })
     })
